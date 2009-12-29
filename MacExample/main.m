b1dbe51c3305681abc9b8164fe501c9a69f43a15
Bruno Conti <53541286+bc0nt13@users.noreply.github.com>
2009-12-28 20:52:01 -0500

    * fixed some warnings and did some anal changes regarding optional object types * changed 'defaultContext' to 'context' since defaultContext implies singleton, want to do same to defaultDevice but that one is bit different * added a super simple mac example

---
+++ MacExample/main.m
@@ -0,0 +1,14 @@
+//
+//  main.m
+//  MacExample
+//
+//  Created by Bruno Conti on 12/28/09.
+//  Copyright 2009 Buttered Cat Software. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, char *argv[])
+{
+    return NSApplicationMain(argc,  (const char **) argv);
+}
